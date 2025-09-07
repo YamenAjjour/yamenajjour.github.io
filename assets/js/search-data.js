169bd10e4284078ff712f0b36535ce20009510d7
YamenAjjour <yamenajjour@hotmail.com>
2025-09-07 13:31:41 +0000

    Deploying to gh-pages from @ YamenAjjour/yamenajjour.github.io@73ac7ad1dc5ce8195a35a2f9762014766f33baa5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -75,16 +75,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-will-be-co-organizing-argmining-2024-in-thailand",
+          title: 'I will be co-organizing ArgMining 2024 in Thailand',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-our-paper-exploring-llms-priming-strategies-for-few-shot-stance-classification-has-been-accepted-at-argmining-2025-in-vienna",
+          title: 'Our Paper Exploring LLMs Priming Strategies for Few-shot Stance Classification has been accepted...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
