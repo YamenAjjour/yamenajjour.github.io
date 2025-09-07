1d26bdd955398cc89635124989f0961371113f3f
YamenAjjour <yamenajjour@hotmail.com>
2025-09-07 14:55:04 +0000

    Deploying to gh-pages from @ YamenAjjour/yamenajjour.github.io@abd62ad262342f33d569642b3ba3c1694a5b6a4c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,39 +63,18 @@ ninja.data = [{
           title: 'Our Paper Exploring LLMs Priming Strategies for Few-shot Stance Classification has been accepted...',
           description: "",
           section: "News",},{
-        id: 'social-email',
-        title: 'email',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://github.com/YamenAjjour", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=PDrc-BUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
