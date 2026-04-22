8d5770dfc67bc7256c4ba5b93fc8a3352d0b521c
YamenAjjour <yamenajjour@hotmail.com>
2026-04-22 07:42:37 +0000

    Deploying to gh-pages from @ YamenAjjour/yamenajjour.github.io@2254ed3caeafec4f3794524998ed341affe034e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -87,8 +87,8 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-exploring-llms-priming-strategies-for-few-shot-stance-classification-has-been-accepted-at-argmining-2025-acl-in-vienna",
           title: 'Our Paper Exploring LLMs Priming Strategies for Few-shot Stance Classification has been accepted...',
           description: "",
-          section: "News",},{id: "news-our-paper-argbench-benchmarking-llms-on-computational-argumentation-tasks-with-carlotta-quensel-nedim-lipka-and-henning-wachsmuth-has-been-accepted-at-acl2026-findings",
-          title: 'Our paper “ArgBench: Benchmarking LLMs on Computational Argumentation Tasks” with Carlotta Quensel, Nedim...',
+          section: "News",},{id: "news-our-paper-argbench-benchmarking-llms-on-computational-argumentation-tasks-has-been-accepted-at-acl2026-findings",
+          title: 'Our paper “ArgBench: Benchmarking LLMs on Computational Argumentation Tasks” has been accepted at...',
           description: "",
           section: "News",},{
         id: 'social-github',
